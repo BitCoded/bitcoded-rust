73503410a7be7d2f74b23c734e5fb0d00f9b63cb
Dylan Rogers <opendylan@proton.me>
2024-01-27 16:49:32 +0000

    0.1.3

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,24 @@
-pub fn add(a: usize, b: usize) -> usize
+pub mod calc
 {
-	a + b
-}
+	pub fn add(a: usize, b: usize) -> usize
+	{
+		a + b
+	}
 
-pub fn sub(a: usize, b: usize) -> usize
-{
-	a - b
-}
+	pub fn sub(a: usize, b: usize) -> usize
+	{
+		a - b
+	}
 
-pub fn mul(a: usize, b: usize) -> usize
-{
-	a * b
-}
+	pub fn mul(a: usize, b: usize) -> usize
+	{
+		a * b
+	}
 
-pub fn div(a: usize, b: usize) -> usize
-{
-	a / b
+	pub fn div(a: usize, b: usize) -> usize
+	{
+		a / b
+	}
 }
 
 
@@ -30,7 +33,7 @@ mod tests
 	#[test]
 	fn test_add()
 	{
-		let result = add(7312543, 41629);
+		let result = calc::add(7312543, 41629);
 		let expect = 7354172;
 		assert_eq!(result, expect);
 	}
@@ -38,7 +41,7 @@ mod tests
 	#[test]
 	fn test_sub()
 	{
-		let result = sub(830, 75);
+		let result = calc::sub(830, 75);
 		let expect = 755;
 		assert_eq!(result, expect);
 	}
@@ -46,7 +49,7 @@ mod tests
 	#[test]
 	fn test_mul()
 	{
-		let result = mul(60, 25);
+		let result = calc::mul(60, 25);
 		let expect = 1500;
 		assert_eq!(result, expect);
 	}
@@ -54,7 +57,7 @@ mod tests
 	#[test]
 	fn test_div()
 	{
-		let result = div(720, 18);
+		let result = calc::div(720, 18);
 		let expect = 40;
 		assert_eq!(result, expect);
 	}
